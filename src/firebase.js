d6b72db7cdca5c961698a8ac0d714e51d90c70a7
Cassandra <cparisi1290@gmail.com>
2021-08-04 15:37:25 -0500

    add firebase code for database

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBydbRGxwkYoPr-LjG0rXClg9CKB18gFKA",
+  authDomain: "clone-2ed1c.firebaseapp.com",
+  projectId: "clone-2ed1c",
+  storageBucket: "clone-2ed1c.appspot.com",
+  messagingSenderId: "912136401871",
+  appId: "1:912136401871:web:7871231e1b09065cdd18ba",
+};
