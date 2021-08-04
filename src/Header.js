7c2e2da395f30849ef5da38143ffd550f39f9434
Cassandra <cparisi1290@gmail.com>
2021-08-04 16:35:43 -0500

    add header component file

---
+++ src/Header.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./header.css";
+
+function Header() {
+  return (
+    <div className="header">
+      <img
+        className="header__logo"
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        alt="Amazon logo"
+      />
+      <div className="header__search">
+        <input className="header_searchInput" type="text" />
+      </div>
+      <div className="header__nav">
+        <div className="header__option">
+          <span className="header__optionLineOne">Hello Guest</span>
+          <span className="header__optionLineTwo">Sign in</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionLineOne">Returns</span>
+          <span className="header__optionLineTwo">& Orders</span>
+        </div>
+        <div className="header__option">
+          <span className="header__optionLineOne">Your</span>
+          <span className="header__optionLineTwo">Prime</span>
+        </div>
+      </div>
+      {/* Children */}
+    </div>
+  );
+}
+
+export default Header;
